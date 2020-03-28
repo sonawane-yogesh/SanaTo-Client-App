a9b14e7afd63eed5c3a43998fec675e5e4995fe5
snehalP <pawarsnehal217@gmail.com>
2020-03-28 16:52:04 +0530

    new fields added to form

--- src/app/helpers/models/data-traning-model.ts
+++ src/app/helpers/models/data-traning-model.ts
@@ -2,6 +2,8 @@ export class DataTrainingMaster {
   DateFormat: string;
   PrimaryKeys: Array<string>;
   NumericFields: Array<string>;
+  NonNumericFields: Array<string>;
+  ProportionalityColumn: string;
   DateColumn: string;
   FileDetails: object;
 
@@ -10,6 +12,9 @@ export class DataTrainingMaster {
     this.DateFormat = data.DateFormat;
     this.NumericFields = data.NumericFields;
     this.PrimaryKeys = data.PrimaryKeys;
+    this.NonNumericFields = data.NonNumericFields;
+    this.ProportionalityColumn = data.ProportionalityColumn;
     this.FileDetails = data.FileDetails;
   }
+  
 }
